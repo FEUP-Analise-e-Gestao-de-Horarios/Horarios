ed8290063571de39aae55dacc697c2f085272235
Claudio Meireles <claudiomeireles555@gmail.com>
2026-03-11 16:48:10 +0000

    Add mirror site script

---
+++ scripts/mirror-side.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+wget --mirror --convert-links --adjust-extension --page-requisites --no-parent \
+  --no-host-directories --cut-dirs=3 \
+  -e robots=off \
+  -P ./site-mirror https://fe.up.pt/horarios/25_26_2s/final/
+
+find site-mirror/ -type f -name "*.html" -exec sed -i 's|</br>|<br>|g' {} +
+
+npx prettier --write "site-mirror/**/*"
