479db606d12cf3aceceffce7b88c85f91e8adc89
Claudio Meireles <claudiomeireles555@gmail.com>
2026-03-11 18:02:29 +0000

    Fix site mirror

--- scripts/serve-mirror-site.sh
+++ scripts/serve-mirror-site.sh
@@ -4,11 +4,5 @@ if [ ! -d "./site-mirror" ] || [ -z "$(ls -A ./site-mirror)" ]; then
   bash scripts/create-mirror-site.sh
 fi
 
-python3 -c "
-from http.server import HTTPServer, SimpleHTTPRequestHandler
-import os
-os.chdir('site-mirror')
-class H(SimpleHTTPRequestHandler):
-    protocol_version = 'HTTP/1.1'
-HTTPServer(('', 8080), H).serve_forever()
-"
+cd ./site-mirror
+python -m http.server 8080
