6778685d910d12b24a41c1f90ad9e8366b4f22e0
Claudio Meireles <claudiomeireles555@gmail.com>
2026-03-12 04:00:37 +0000

    Updates all views to use new User and Project models

--- src/parser/urls.py
+++ src/parser/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
+
 from . import views
 
 app_name = "parser"
 
 urlpatterns = [
-    path('parse/', views.parse),
-    path('selecionar_aulas_em_paralelo/', views.selecionar_aulas_em_paralelo, name='selecionar_aulas_em_paralelo'),
-    path('guardar_aulas_em_paralelo/', views.guardar_aulas_em_paralelo, name='guardar_aulas_em_paralelo'),
+    path(
+        "selecionar_aulas_em_paralelo/",
+        views.selecionar_aulas_em_paralelo,
+        name="selecionar_aulas_em_paralelo",
+    ),
+    path(
+        "guardar_aulas_em_paralelo/",
+        views.guardar_aulas_em_paralelo,
+        name="guardar_aulas_em_paralelo",
+    ),
 ]
