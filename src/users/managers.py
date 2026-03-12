07bc50d0cc9ea4a73cc79daa60f326ce1bd01b3a
Claudio Meireles <claudiomeireles555@gmail.com>
2026-03-12 04:00:37 +0000

    Renames CustomUser to User and cleans up user model

--- src/users/managers.py
+++ src/users/managers.py
@@ -2,11 +2,12 @@ from django.contrib.auth.base_user import BaseUserManager
 from django.utils.translation import gettext_lazy as _
 
 
-class CustomUserManager(BaseUserManager):
+class UserManager(BaseUserManager):
     """
     Custom user model manager where email is the unique identifiers
     for authentication instead of usernames.
     """
+
     def create_user(self, email, username, password, **extra_fields):
         """
         Create and save a user with the given email and password.
@@ -14,7 +15,7 @@ class CustomUserManager(BaseUserManager):
         if not email:
             raise ValueError(_("The Email must be set"))
         email = self.normalize_email(email)
-        user = self.model(username=username, email = email, **extra_fields)
+        user = self.model(username=username, email=email, **extra_fields)
         user.set_password(password)
         user.save()
         return user
