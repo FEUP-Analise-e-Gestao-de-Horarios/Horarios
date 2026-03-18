2730bb35a2afc63f4e1e20e7dc6dbafbec5bca7a
Claudio Meireles <claudiomeireles555@gmail.com>
2026-03-18 21:13:57 +0000

    Add formatting to base DAO class

--- src/projects/projects_db/dao/base_dao.py
+++ src/projects/projects_db/dao/base_dao.py
@@ -8,6 +8,7 @@ from src.projects.projects_db.base import Base
 T = TypeVar("T", bound=Base)
 
 
+
 class BaseDAO[T]:
     def __init__(self, model: type[T], session: Session) -> None:
         self.model = model
