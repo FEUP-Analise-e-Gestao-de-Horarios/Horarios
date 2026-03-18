9ffa96a10a6809fa0bbab69db59b9c5afc4e55a7
Claudio Meireles <claudiomeireles555@gmail.com>
2026-03-18 21:13:57 +0000

    Enhance DAO classes with bulk query methods

--- src/projects/projects_db/dao/subject_dao.py
+++ src/projects/projects_db/dao/subject_dao.py
@@ -4,6 +4,7 @@ from sqlalchemy import select
 from sqlalchemy.orm import Session
 
 from src.projects.projects_db.dao.base_dao import BaseDAO
+from src.projects.projects_db.dao.exceptions import MultipleNotFoundError
 from src.projects.projects_db.models.degree import Degree
 from src.projects.projects_db.models.subject import Subject
 from src.projects.projects_db.models.year import Year
@@ -30,6 +31,18 @@ class SubjectDAO(BaseDAO[Subject]):
     def get_by_code(self, code: str) -> Subject | None:
         return self.session.scalars(select(Subject).where(Subject.code == code)).one_or_none()
 
+    def get_by_numbers(self, numbers: set[int], *, check_count: bool = True) -> list[Subject]:
+        if not numbers:
+            return []
+
+        subjects = list(self.session.scalars(select(Subject).where(Subject.number.in_(numbers))).all())
+        if check_count and len(numbers) != len(subjects):
+            found = {s.number for s in subjects}
+            missing = numbers - found
+            raise MultipleNotFoundError("number", missing)
+
+        return subjects
+
     def get_by_degree_and_year(self, *, degree_acronym: str, year_number: int) -> list[Subject]:
         return list(
             self.session.scalars(
@@ -39,3 +52,5 @@ class SubjectDAO(BaseDAO[Subject]):
                 .where(Degree.acronym == degree_acronym, Year.number == year_number),
             ).all(),
         )
+
+    
