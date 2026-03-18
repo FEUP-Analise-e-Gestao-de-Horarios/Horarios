a8bfb96b21e92a33bd0ea29afca19f6e64c9d299
Claudio Meireles <claudiomeireles555@gmail.com>
2026-03-18 21:56:01 +0000

    Revert changes to utils.py

--- src/parser/utils.py
+++ src/parser/utils.py
@@ -1,17 +1,18 @@
-from django.http import HttpResponse
+from django.http import JsonResponse
 from pydantic import BaseModel, ValidationError
 
 
-# TODO return user friendly errors
 def validate_request_body[M: BaseModel](
     model: type[M],
     body: bytes,
-) -> tuple[M, None] | tuple[None, HttpResponse]:
+) -> tuple[M, None] | tuple[None, JsonResponse]:
     try:
         return model.model_validate_json(body), None
     except ValidationError as e:
-        return None, HttpResponse(
-            e.json(),
-            content_type="application/json",
+        return None, JsonResponse(
+            {
+                "status": "erro",
+                "message": e.errors(include_input=False, include_url=False),
+            },
             status=400,
         )
