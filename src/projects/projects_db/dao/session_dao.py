1a8fa0602d14baf70329f7e2e9cc9d3f366adde1
Claudio Meireles <claudiomeireles555@gmail.com>
2026-03-18 21:52:19 +0000

    Remove some methods

--- src/projects/projects_db/dao/session_dao.py
+++ src/projects/projects_db/dao/session_dao.py
@@ -13,10 +13,8 @@ from src.projects.projects_db.models._secondary_tables import (
     session_teachers,
 )
 from src.projects.projects_db.models.class_ import Class
-from src.projects.projects_db.models.room import Room
 from src.projects.projects_db.models.session import Session
 from src.projects.projects_db.models.subject import Subject
-from src.projects.projects_db.models.teacher import Teacher
 from src.projects.projects_db.schemas.weekday import WeekDay
 
 
@@ -78,29 +76,6 @@ class SessionDAO(BaseDAO[Session]):
     # -------------------------------------------------------------------
     # -- Get
     # -------------------------------------------------------------------
-
-    def get_by_week(self, week: datetime.date) -> list[Session]:
-        return list(
-            self.session.scalars(
-                select(Session).where(Session.week == week),
-            ).all(),
-        )
-
-    def get_by_room(self, room_id: UUID) -> list[Session]:
-        room = self.session.get(Room, room_id)
-        return room.sessions if room else []
-
-    def get_by_teacher(self, teacher_id: UUID) -> list[Session]:
-        teacher = self.session.get(Teacher, teacher_id)
-        return teacher.sessions if teacher else []
-
-    def get_by_subject(self, subject_id: UUID) -> list[Session]:
-        subject = self.session.get(Subject, subject_id)
-        return subject.sessions if subject else []
-
-    def get_by_class(self, class_id: UUID) -> list[Session]:
-        class_ = self.session.get(Class, class_id)
-        return class_.sessions if class_ else []
 
     def get_by_subject_type(
         self,
