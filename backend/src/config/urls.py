cde9b49af20242979816c032c214cc63d6cffe10
Claudio Meireles <claudiomeireles555@gmail.com>
2026-03-20 16:13:14 +0000

    Adjust routes

--- backend/src/config/urls.py
+++ backend/src/config/urls.py
@@ -19,8 +19,6 @@ from django.urls import include, path
 from django.views.decorators.csrf import ensure_csrf_cookie
 from django.views.generic import TemplateView
 
-from src.FeupScheduleEditor import views
-
 # SPA (Single Page Application) for React
 spa_view = ensure_csrf_cookie(
     TemplateView.as_view(template_name="index.html"),
@@ -31,53 +29,39 @@ urlpatterns = [
     # React URLs
     path("", spa_view, name="dashboard"),
     path("login", spa_view, name="login"),
-    path("react-dashboard/", spa_view, name="react-dashboard"),
-     path("react-schedule/", spa_view, name="react-schedule"),
-
+    path("/forgot-password", spa_view, name="forgot-password"),
+    path("/change-password", spa_view, name="change-password"),
+    path("/projects/<int:project_id>/", spa_view, name="react-schedule"),
     # API endpoints
-    path("admin/", admin.site.urls, name="admin"),  # TODO Change
     path("api/projects/", include("src.projects.urls")),
     path("api/auth/", include("src.login.urls")),
-    # TODO: Check URLs bellow
-    path("parser/", include("src.parser.urls")),
-    path("groups", views.groups),
-    path("deleteProject", views.deleteProject),
-    path("editturnos/<int:projId>", views.editTurnos),
-    path("emptytable/", views.createEmptyTable, name="emptytable"),
-    path("getucs/", views.get_uc_list, name="get_uc_list"),
-    path("table/", views.fillPageForCursoAno, name="table"),
-    path("distribuicao/", views.distribuicao_view, name="distribuicao"),
-    path("schedule/", views.schedule_view, name="schedule"),
-    path("blocosturma/", views.blocosVermelhosTurma, name="blocosturma"),
-    path("getdocentehorario/", views.get_docente_horario, name="getdocentehorario"),
-    path("getsalahorario/", views.get_sala_horario, name="getsalahorario"),
-    path(
-        "getdocenteminihorario",
-        views.getDocenteMiniHorario,
-        name="getdocenteminihorario",
-    ),
-    path("getsalaminihorario", views.getSalaMiniHorario, name="getsalaminihorario"),
-    path("editturnos/<int:projId>/createDocente/", views.createDocente),
-    path("editturnos/<int:projId>/editDocentes/", views.editDocentes),
-    path(
-        "editturnos/<int:projId>/editDocentes/makeChange/",
-        views.editDocentesMakeChange,
-    ),
-    path("editturnos/<int:projId>/makechanges", views.makeChanges),
-    path("manageProjects/<int:projId>", views.manageProjects),
-    path("conflicts/<int:projId>/", views.getConflicts, name="conflicts_page"),
-    path("export/<int:projId>", views.export),
-    path("ucview/<int:projId>/<str:uc_codigo>", views.uc_view, name="uc_view"),
-    path("editturnos/<int:projId>/uc_changes/", views.uc_changes, name="uc_changes"),
-    path(
-        "editturnos/<int:projId>/swap_teachers/",
-        views.swap_teachers,
-        name="swap_teachers",
-    ),
-    path(
-        "getaulaparalelosimultanea",
-        views.getAulaSimultaneasParalelas,
-        name="getaulaparalelosimultanea",
-    ),
-    path("editturnos/<int:projId>/swap_aulas/", views.swap_aulas, name="swap_aulas"),
+    path("admin/", admin.site.urls, name="admin"),  # TODO Check URL
 ]
+
+# TODO Check URLs bellow
+# path("parser/", include("src.parser.urls")),
+# path("groups", views.groups),
+# path("deleteProject", views.deleteProject),
+# path("editturnos/<int:projId>", views.editTurnos),
+# path("emptytable/", views.createEmptyTable, name="emptytable"),
+# path("getucs/", views.get_uc_list, name="get_uc_list"),
+# path("table/", views.fillPageForCursoAno, name="table"),
+# path("distribuicao/", views.distribuicao_view, name="distribuicao"),
+# path("schedule/", views.schedule_view, name="schedule"),
+# path("blocosturma/", views.blocosVermelhosTurma, name="blocosturma"),
+# path("getdocentehorario/", views.get_docente_horario, name="getdocentehorario"),
+# path("getsalahorario/", views.get_sala_horario, name="getsalahorario"),
+# path("getdocenteminihorario", views.getDocenteMiniHorario, name="getdocenteminihorario"),
+# path("getsalaminihorario", views.getSalaMiniHorario, name="getsalaminihorario"),
+# path("editturnos/<int:projId>/createDocente/", views.createDocente),
+# path("editturnos/<int:projId>/editDocentes/", views.editDocentes),
+# path("editturnos/<int:projId>/editDocentes/makeChange/", views.editDocentesMakeChange),
+# path("editturnos/<int:projId>/makechanges", views.makeChanges),
+# path("manageProjects/<int:projId>", views.manageProjects),
+# path("conflicts/<int:projId>/", views.getConflicts, name="conflicts_page"),
+# path("export/<int:projId>", views.export),
+# path("ucview/<int:projId>/<str:uc_codigo>", views.uc_view, name="uc_view"),
+# path("editturnos/<int:projId>/uc_changes/", views.uc_changes, name="uc_changes"),
+# path("editturnos/<int:projId>/swap_teachers/", views.swap_teachers, name="swap_teachers"),
+# path( "getaulaparalelosimultanea", views.getAulaSimultaneasParalelas, name="getaulaparalelosimultanea"),
+# path("editturnos/<int:projId>/swap_aulas/", views.swap_aulas, name="swap_aulas"),
--- frontend/src/routes.ts
+++ frontend/src/routes.ts
@@ -2,5 +2,5 @@ export const ROUTES = {
   HOME: "/",
   LOGIN: "/login",
   FORGOT_PASSWORD: "/forgot-password",
-  CHANGE_PASSWORD: "/react-change-password",
+  CHANGE_PASSWORD: "/change-password",
 } as const;
