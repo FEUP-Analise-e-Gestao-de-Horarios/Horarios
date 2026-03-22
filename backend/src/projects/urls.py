940b075008f5f9437b2dce418b95d9a289f0132d
Claudio Meireles <claudiomeireles555@gmail.com>
2026-03-22 00:14:58 +0000

    Adjust URLs

--- backend/src/projects/urls.py
+++ backend/src/projects/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import include, path
 
 from src.projects.views import (
     ProjectClassesView,
@@ -16,22 +16,36 @@ from src.projects.views import (
 
 app_name = "projects"
 
+year_patterns = [
+    path("", ProjectYearsView.as_view()),
+    path("<uuid:year_id>/subjects/", ProjectSubjectsView.as_view()),
+    path("<uuid:year_id>/classes/", ProjectClassesView.as_view()),
+]
+
+degree_patterns = [
+    path("", ProjectDegreesView.as_view()),
+    path("<uuid:degree_id>/years/", include(year_patterns)),
+]
+
+teacher_patterns = [
+    path("", ProjectTeachersView.as_view()),
+    path("<uuid:teacher_id>", ProjectTeacherView.as_view()),
+]
+
+room_patterns = [
+    path("", ProjectRoomsView.as_view()),
+    path("<uuid:room_id>", ProjectRoomView.as_view()),
+]
+
+project_patterns = [
+    path("", ProjectView.as_view()),
+    path("stats", ProjectStatsView.as_view()),
+    path("rooms/", include(room_patterns)),
+    path("teachers/", include(teacher_patterns)),
+    path("degrees/", include(degree_patterns)),
+]
+
 urlpatterns = [
     path("", ProjectsView.as_view()),
-    path("<int:project_id>", ProjectView.as_view()),
-    path("<int:project_id>/stats", ProjectStatsView.as_view()),
-    path("<int:project_id>/rooms/", ProjectRoomsView.as_view()),
-    path("<int:project_id>/rooms/<uuid:room_id>", ProjectRoomView.as_view()),
-    path("<int:project_id>/teachers/", ProjectTeachersView.as_view()),
-    path("<int:project_id>/teachers/<uuid:teacher_id>", ProjectTeacherView.as_view()),
-    path("<int:project_id>/degrees/", ProjectDegreesView.as_view()),
-    path("<int:project_id>/degrees/<uuid:degree_id>/years/", ProjectYearsView.as_view()),
-    path(
-        "<int:project_id>/degrees/<uuid:degree_id>/years/<uuid:year_id>/subjects/",
-        ProjectSubjectsView.as_view(),
-    ),
-    path(
-        "<int:project_id>/degrees/<uuid:degree_id>/years/<uuid:year_id>/classes/",
-        ProjectClassesView.as_view(),
-    ),
+    path("<int:project_id>", include(project_patterns)),
 ]
