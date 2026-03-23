d1d5b564d8d6afd2177a0beb16e410f23a0b33d4
Claudio Meireles <claudiomeireles555@gmail.com>
2026-03-23 18:49:04 +0000

    Remove session_classes uses

--- backend/src/projects/projects_db/dao/degree_dao.py
+++ backend/src/projects/projects_db/dao/degree_dao.py
@@ -4,10 +4,10 @@ from sqlalchemy import distinct, func, select
 from sqlalchemy.orm import Session
 
 from src.projects.projects_db.dao.base_dao import BaseDAO
-from src.projects.projects_db.models._secondary_tables import session_subjects
 from src.projects.projects_db.models.class_ import Class
 from src.projects.projects_db.models.degree import Degree
 from src.projects.projects_db.models.session import Session as SessionModel
+from src.projects.projects_db.models.session_class_subject import SessionClassSubject
 from src.projects.projects_db.models.subject import Subject
 from src.projects.projects_db.models.year import Year
 from src.projects.projects_db.schemas.degree import DegreeStats
@@ -66,8 +66,8 @@ class DegreeDAO(BaseDAO[Degree]):
         sessions_sq = (
             select(Year.degree_id, func.count(distinct(SessionModel.id)).label("cnt"))
             .join(Subject, Subject.year_id == Year.id)
-            .join(session_subjects, session_subjects.c.subject_id == Subject.id)
-            .join(SessionModel, SessionModel.id == session_subjects.c.session_id)
+            .join(SessionClassSubject, SessionClassSubject.subject_id == Subject.id)
+            .join(SessionModel, SessionModel.id == SessionClassSubject.session_id)
             .group_by(Year.degree_id)
             .subquery()
         )
@@ -119,8 +119,8 @@ class DegreeDAO(BaseDAO[Degree]):
         sessions_sq = (
             select(Year.degree_id, func.count(distinct(SessionModel.id)).label("cnt"))
             .join(Subject, Subject.year_id == Year.id)
-            .join(session_subjects, session_subjects.c.subject_id == Subject.id)
-            .join(SessionModel, SessionModel.id == session_subjects.c.session_id)
+            .join(SessionClassSubject, SessionClassSubject.subject_id == Subject.id)
+            .join(SessionModel, SessionModel.id == SessionClassSubject.session_id)
             .group_by(Year.degree_id)
             .subquery()
         )
